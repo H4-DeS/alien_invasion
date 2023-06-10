916d2a0fffbd3f32acdc56d7efe0602cd591823f
H4-Des <oliveira.gonzales@gmail.com>
2023-06-10 13:15:12 -0300

    bullets created

--- settings.py
+++ settings.py
@@ -7,3 +7,7 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (230,230,230)
         self.ship_speed = 2.0
+        self.bullet_width = 2
+        self.bullet_height = 15
+        self.bullet_speed = 2.0
+        self.bullet_color = (60,60,60)
